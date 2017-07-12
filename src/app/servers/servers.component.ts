616ee96c5418e7c1d6e0712f521532027e189ac4
Navneet Sharma <sharma.navneet1988@gmail.com>
2017-07-13 02:26:07 +0530

    Components & Databinding

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  // selector: '[app-servers]',               // attribute selector
+  // selector: '.app-servers',                // class selector
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver ';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+}
